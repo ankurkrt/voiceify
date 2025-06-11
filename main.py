3451251256eb7a055986c9aa9968bcb8659af0c6
Ankur Kr <ankur.net111@gmail.com>
2025-06-11 05:46:51 +0000

    test code

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World !!")
+import speech_recognition as sr
+
+def speech_to_text():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak something...")
+        audio = recognizer.listen(source)
+        try:
+            text = recognizer.recognize_google(audio)
+            print("You said: {}".format(text))
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+
+if __name__ == "__main__":
+    speech_to_text()
